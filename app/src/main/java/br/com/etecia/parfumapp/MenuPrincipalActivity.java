6e3836d20d65ccd12b1b915bf5ff18eeb50b5f88
otaviolt <otavio.torraca@etec.sp.gov.br>
2024-09-19 21:59:03 -0300

    cliques

--- app/src/main/java/br/com/etecia/parfumapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/parfumapp/MenuPrincipalActivity.java
@@ -1,14 +1,20 @@
 package br.com.etecia.parfumapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuPrincipalActivity extends AppCompatActivity {
+public class MenuPrincipalActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CardView PerfumesMasculinos, PerfumesFemininos, Hidratantes, BodySplash;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,33 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        PerfumesMasculinos = findViewById(R.id.masculino);
+        PerfumesFemininos = findViewById(R.id.feminino);
+        Hidratantes = findViewById(R.id.hidratante);
+        BodySplash = findViewById(R.id.bodysplash);
+
+        PerfumesMasculinos.setOnClickListener(this);
+        PerfumesFemininos.setOnClickListener(this);
+        Hidratantes.setOnClickListener(this);
+        BodySplash.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.masculino) {
+            startActivity(new Intent(getApplicationContext(), PerfumesMasculinosActivity.class));
+        }
+        if (view.getId() == R.id.feminino) {
+            startActivity(new Intent(getApplicationContext(), PerfumesFemininosActivity.class));
+        }
+        if (view.getId() == R.id.hidratante) {
+            startActivity(new Intent(getApplicationContext(), HidratantesActivity.class));
+        }
+        if (view.getId() == R.id.bodysplash) {
+            startActivity(new Intent(getApplicationContext(), BodySplashActivity.class));
+        }
+
     }
 }
