a8253f08d0d386ff5dffd1fa81e4c4e8c1d4f07b
otaviolt <otavio.torraca@etec.sp.gov.br>
2024-10-10 19:34:37 -0300

    toolbar

--- app/src/main/java/br/com/etecia/parfumapp/PerfumesMasculinosActivity.java
+++ app/src/main/java/br/com/etecia/parfumapp/PerfumesMasculinosActivity.java
@@ -1,14 +1,25 @@
 package br.com.etecia.parfumapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.tabs.TabLayout;
 
 public class PerfumesMasculinosActivity extends AppCompatActivity {
+    TabLayout idTabLayoutPerfumeLacrado;
+    ViewPager2 idViewPagerPerfumes;
+
+    MaterialToolbar idToolBarPerfumeMasculino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,17 @@ public class PerfumesMasculinosActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        idToolBarPerfumeMasculino = findViewById(R.id.idToolBarPerfumeMasculino);
+        idToolBarPerfumeMasculino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MenuPrincipalActivity.class));
+                finish();
+            }
+        });
+
     }
 }
