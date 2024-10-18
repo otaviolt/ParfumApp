7242dc30664c12fa21305deae6d32a97fdc48095
otaviolt <otavio.torraca@etec.sp.gov.br>
2024-10-17 21:16:32 -0300

    atualizado

--- app/src/main/java/br/com/etecia/parfumapp/PerfumesLacradosFragment.java
+++ app/src/main/java/br/com/etecia/parfumapp/PerfumesLacradosFragment.java
@@ -10,19 +10,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PerfumesLacradosFragment extends Fragment {
+
+    private List<PerfumeLacrado> lstPerfumeLacrado;
+    RecyclerView idRecPerfumesLacrados;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
         View view = inflater.inflate(R.layout.fragment_perfumes_lacrados, container, false);
+
+        idRecPerfumesLacrados = view.findViewById(R.id.idRecPerfumesLacrados);
+
+        //carregando os objetos na lista
+        lstPerfumeLacrado = new ArrayList<>();
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.khamrah));
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.invictus));
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.ultramale));
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.hugoboss));
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.heroes));
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.black));
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.eros));
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.phantom));
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.azzaro));
+        lstPerfumeLacrado.add(new PerfumeLacrado("Lattafa Khamrah", R.drawable.ani));
+
+        //carregando o adaptador
+        RecyclerAdapterPerfumeLacrado adapterPerfumeLacrado = new RecyclerAdapterPerfumeLacrado()
+
 
         return view;
 
 
-
-        }
     }
+}
--- app/src/main/java/br/com/etecia/parfumapp/RecyclerAdapterPerfumeLacrado.java
+++ app/src/main/java/br/com/etecia/parfumapp/RecyclerAdapterPerfumeLacrado.java
@@ -1,5 +1,7 @@
 package br.com.etecia.parfumapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,22 +11,46 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class RecyclerAdapterPerfumeLacrado extends RecyclerView.Adapter<RecyclerAdapterPerfumeLacrado.ViewHolder> {
+
+    //criando variaveis globais para representar a lista e o contexto
+    private Context context;
+    private List<PerfumeLacrado> lstPerfumeLacrado;
+
+
+    //criando o construtor
+    public RecyclerAdapterPerfumeLacrado(Context context, List<PerfumeLacrado> lstPerfumeLacrado) {
+        this.context = context;
+        this.lstPerfumeLacrado = lstPerfumeLacrado;
+    }
+
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.modelo_perfumes_lacrados,parent,false);
+
+        return new ViewHolder(view);
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.idModeloTituloPerfumeLacrado.setText(lstPerfumeLacrado.get(position).getTitulo());
+        holder.idModeloImagemPerfumeLacrado.setImageResource(lstPerfumeLacrado.get(position).getImagem());
+
+        //criar o clique no card
 
     }
 
+
     @Override
     public int getItemCount() {
-        return 0;
+        return lstPerfumeLacrado.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
